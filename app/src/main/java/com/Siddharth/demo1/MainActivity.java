ae35497da0e63c97502ac37202526a58592f831e
James-Paradox <siddharthtripathi753@gmail.com>
2020-02-18 15:34:52 +0530

    this

--- app/src/main/java/com/Siddharth/demo1/MainActivity.java
+++ app/src/main/java/com/Siddharth/demo1/MainActivity.java
@@ -3,12 +3,41 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"OnCreate",Toast.LENGTH_LONG).show();
+
     }
-}
+
+     @Override
+     protected void onStart() {
+         super.onStart();
+         Toast.makeText(MainActivity.this,"OnStart",Toast.LENGTH_LONG).show();
+     }
+
+     @Override
+     protected void onStop() {
+        super.onStop();
+         Toast.makeText(MainActivity.this,"OnStop",Toast.LENGTH_LONG).show();
+    }
+
+     @Override
+     protected void onDestroy() {
+         super.onDestroy();
+         Toast.makeText(MainActivity.this,"OnDestroy",Toast.LENGTH_LONG).show();
+     }
+
+     @Override
+     protected void onPause() {
+         super.onPause();
+         Toast.makeText(MainActivity.this,"OnPause",Toast.LENGTH_LONG).show();
+     }
+ }
+
+
